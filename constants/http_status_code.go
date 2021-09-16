89b1151ab3d16195a2719c53592f8f0147991900
Wellington <wellington.junior@madeiramadeira.com.br>
2021-09-16 15:26:12 -0300

    feat: cielo http status code

---
+++ constants/http_status_code.go
@@ -0,0 +1,17 @@
+package constants
+
+import "net/http"
+
+const (
+	// HTTP_STATUS_OK [200, 2001] ...
+	HTTP_STATUS_OK = http.StatusOK
+
+	// HTTP_STATUS_OK // [400] ...
+	HTTP_STATUS_BAD_REQUEST = http.StatusBadRequest
+
+	// HTTP_STATUS_RESOURCE_NOT_FOUND [404] ...
+	HTTP_STATUS_RESOURCE_NOT_FOUND = http.StatusNotFound
+
+	// HTTP_STATUS_INTERVAL_SERVER_ERROR [500]...
+	HTTP_STATUS_INTERVAL_SERVER_ERROR = http.StatusInternalServerError
+)
