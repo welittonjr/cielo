c6bffb55f94caa550791ca3f2958ed375abacce0
Wellington <wellington.junior@madeiramadeira.com.br>
2021-09-16 19:24:03 -0300

    feat: request para api

--- requests/request.go
+++ requests/request.go
@@ -2,8 +2,12 @@ package requests
 
 import (
 	"bytes"
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/welittonjr/cielo/constants"
+	"github.com/welittonjr/cielo/errors"
 )
 
 // Request encapsula todas as informações necessárias
@@ -34,33 +38,69 @@ func (request *Request) addRequestHeaders(req *http.Request, headers map[string]
 	request.addRequestHeadersInternal(req, headers)
 }
 
+func processResponse(response *http.Response) (map[string]interface{}, error) {
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	resp := make(map[string]interface{})
+
+	if err := json.Unmarshal(body, &resp); err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
 // Fazer Request
-func (request *Request) doRequest(req *http.Request) {
+func (request *Request) doRequest(req *http.Request) (map[string]interface{}, error) {
 
 	client := &http.Client{}
 	response, err := client.Do(req)
 
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
-	defer response.Body.Close()
 
-	// if response.StatusCode >= constants.HTTP_STATUS_OK {
+	if response.StatusCode == constants.HTTP_STATUS_OK {
+		return processResponse(response)
+	}
 
-	// }
+	// Gera um erro dependendo do tipo de erro na resposta
+	var responseError errors.CieloError
+	body, _ := ioutil.ReadAll(response.Body)
+	json.Unmarshal(body, &responseError)
+	errorCielo := responseError[0]
 
-	// Debugs
-	// body, _ := ioutil.ReadAll(response.Body)
-	// fmt.Println()
-	fmt.Println("response Status:", response.StatusCode)
-	// fmt.Println()
-	// fmt.Println("response Headers:", response.Header)
-	// fmt.Println()
-	// fmt.Println("response JSON Body:", string(body))
+	switch response.StatusCode {
+	case constants.HTTP_STATUS_INTERVAL_SERVER_ERROR:
+		return nil, &errors.InternalServerError{
+			Message: errorCielo.Message,
+			Code:    errorCielo.Code,
+		}
+
+	case constants.HTTP_STATUS_RESOURCE_NOT_FOUND:
+		return nil, &errors.ResourceNotFound{
+			Message: errorCielo.Message,
+			Code:    errorCielo.Code,
+		}
+
+	case constants.HTTP_STATUS_BAD_REQUEST:
+		return nil, &errors.BadRequestError{
+			Message: errorCielo.Message,
+			Code:    errorCielo.Code,
+		}
+	}
+
+	return processResponse(response)
 }
 
 // Post ...
-func (request *Request) Post(url string, payload string, headers map[string]string) {
+func (request *Request) Post(url string, payload string, headers map[string]string) (map[string]interface{}, error) {
 
 	jsonStr := []byte(payload)
 
@@ -70,5 +110,5 @@ func (request *Request) Post(url string, payload string, headers map[string]stri
 	request.addRequestHeaders(req, headers)
 
 	// Realizar Requisição
-	request.doRequest(req)
+	return request.doRequest(req)
 }
